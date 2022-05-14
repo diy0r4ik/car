13a387bab319b9a5122fced5664e08dd4b4e002d
diy0r4ik <diyorbekabdusalomov05@gmail.com>
2022-05-14 16:15:26 +0300

    h
    srthz

--- js/abbas.js
+++ js/abbas.js
@@ -1,41 +1,78 @@
 
+const text1_options = [
+  "Range Rover S64 Coupe",
+  "Range Rover S64 Coupe",
+  "Range Rover S64 Coupe",
+  "Range Rover S64 Coupe"
+];
+const text2_options = [
+  "$250",
+  "$250",
+  "$250",
+  "$250"
+];
 
-// ABBAS CAROUSEL./
+const color_options = ["#181717", "#181717", "#181717", "#181717"];
+const image_options = [
+  "images/img_1.jpg",
+  "images/img_2.jpg",
+  "images/img_3.jpg",
+  "images/img_4.jpg"
+];
+var i = 0;
+const currentOptionText1 = document.getElementById("current-option-text1");
+const currentOptionText2 = document.getElementById("current-option-text2");
+const currentOptionImage = document.getElementById("image");
+const carousel = document.getElementById("carousel-wrapper");
+const mainMenu = document.getElementById("menu");
+const optionPrevious = document.getElementById("previous-option");
+const optionNext = document.getElementById("next-option");
 
+currentOptionText1.innerText = text1_options[i];
+currentOptionText2.innerText = text2_options[i];
+currentOptionImage.style.backgroundImage = "url(" + image_options[i] + ")";
+mainMenu.style.background = color_options[i];
 
-$(function(){
-  $('.carousel-item').eq(0).addClass('active');
-  var total = $('.carousel-item').length;
-  var current = 0;
-  $('#moveRight').on('click', function(){
-    var next=current;
-    current= current+1;
-    setSlide(next, current);
-  });
-  $('#moveLeft').on('click', function(){
-    var prev=current;
-    current = current- 1;
-    setSlide(prev, current);
-  });
-  function setSlide(prev, next){
-    var slide= current;
-    if(next>total-1){
-     slide=0;
-      current=0;
-    }
-    if(next<0){
-      slide=total - 1;
-      current=total - 1;
-    }
-           $('.carousel-item').eq(prev).removeClass('active');
-           $('.carousel-item').eq(slide).addClass('active');
-      setTimeout(function(){
+optionNext.onclick = function () {
+  i = i + 1;
+  i = i % text1_options.length;
+  currentOptionText1.dataset.nextText = text1_options[i];
 
-      },800);
-    
+  currentOptionText2.dataset.nextText = text2_options[i];
 
-    
-    console.log('current '+current);
-    console.log('prev '+prev);
+  mainMenu.style.background = color_options[i];
+  carousel.classList.add("anim-next");
+  
+  setTimeout(() => {
+    currentOptionImage.style.backgroundImage = "url(" + image_options[i] + ")";
+  }, 455);
+  
+  setTimeout(() => {
+    currentOptionText1.innerText = text1_options[i];
+    currentOptionText2.innerText = text2_options[i];
+    carousel.classList.remove("anim-next");
+  }, 650);
+};
+
+optionPrevious.onclick = function () {
+  if (i === 0) {
+    i = text1_options.length;
   }
-});
+  i = i - 1;
+  currentOptionText1.dataset.previousText = text1_options[i];
+
+  currentOptionText2.dataset.previousText = text2_options[i];
+
+  mainMenu.style.background = color_options[i];
+  carousel.classList.add("anim-previous");
+
+  setTimeout(() => {
+    currentOptionImage.style.backgroundImage = "url(" + image_options[i] + ")";
+  }, 455);
+  
+  setTimeout(() => {
+    currentOptionText1.innerText = text1_options[i];
+    currentOptionText2.innerText = text2_options[i];
+    carousel.classList.remove("anim-previous");
+  }, 650);
+};
