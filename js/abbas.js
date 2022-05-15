63d41406eeeb50b19ec5a81734ec05163687d028
diy0r4ik <95129772+diy0r4ik@users.noreply.github.com>
2022-05-15 18:18:39 +0500

    qeasfdgbcvnb
    rhetjyuy

--- js/abbas.js
+++ js/abbas.js
@@ -76,3 +76,28 @@ optionPrevious.onclick = function () {
     carousel.classList.remove("anim-previous");
   }, 650);
 };
+jQuery(document).ready(function($) {
+  "use strict";
+  //  TESTIMONIALS CAROUSEL HOOK
+  $('#customers-testimonials').owlCarousel({
+      loop: true,
+      center: true,
+      items: 3,
+      margin: 0,
+      autoplay: true,
+      dots:true,
+      autoplayTimeout: 8500,
+      smartSpeed: 450,
+      responsive: {
+        0: {
+          items: 1
+        },
+        768: {
+          items: 2
+        },
+        1170: {
+          items: 3
+        }
+      }
+  });
+});
